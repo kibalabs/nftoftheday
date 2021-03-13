68f2b2ab1ea81d1a0cbf6ab375334fea408439d3
Krishan Patel <krishan711@gmail.com>
2021-03-13 09:47:07 +0000

    .

--- api/notd/api/api_v1.py
+++ api/notd/api/api_v1.py
@@ -9,7 +9,7 @@ from notd.core.util import date_util
 def create_api(notdManager: NotdManager) -> KibaRouter():
     router = KibaRouter()
 
-    @router.get('/retrieve-ui-data', response_model=RetrieveUiDataResponse)
+    @router.post('/retrieve-ui-data', response_model=RetrieveUiDataResponse)
     async def retrieve_ui_data(rawRequest: Request, response: Response, startDate: Optional[datetime.datetime] = None, endDate: Optional[datetime.datetime] = None):  # request: RetrieveUiDataRequest
         startDate = startDate or date_util.start_of_day(dt=datetime.datetime.now())
         endDate = endDate or date_util.start_of_day(dt=date_util.datetime_from_datetime(dt=datetime.datetime.now(), days=1))
