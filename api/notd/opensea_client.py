5b7cd50a2bd2827304cf309da0d0c4c5b2201164
Krishan Patel <krishan711@gmail.com>
2021-04-08 23:55:50 +0100

    .

--- api/notd/opensea_client.py
+++ api/notd/opensea_client.py
@@ -28,15 +28,15 @@ class OpenseaClient:
                 retryCount += 1
         responseJson = response.json()
         registryToken = RegistryToken(
-            name=responseJson['name'] or f"{responseJson['collection']['name']} #{tokenId}",
-            imageUrl=responseJson['animation_original_url'] or responseJson['animation_url'] or responseJson['image_url'] or responseJson.get('original_image_url'),
+            name=responseJson.get('name') or f"{responseJson.get('collection', {}).get('name')} #{tokenId}",
+            imageUrl=responseJson.get('animation_original_url') or responseJson.get('animation_url') or responseJson.get('image_url') or responseJson.get('original_image_url') or responseJson.get('image_original_url'),
             openSeaUrl=f"{responseJson['permalink']}?ref=0x18090cda49b21deaffc21b4f886aed3eb787d032",
-            externalUrl=responseJson['external_link'],
+            externalUrl=responseJson.get('external_link'),
             lastSaleDate=date_util.datetime_from_string(dateString=responseJson['last_sale']['created_date']) if responseJson.get('last_sale') else None,
             lastSalePrice=int(responseJson['last_sale']['total_price']) if responseJson.get('last_sale') else None,
-            collectionName=responseJson['collection']['name'],
-            collectionImageUrl=responseJson['collection']['large_image_url'] or responseJson['collection']['image_url'],
-            collectionOpenSeaUrl=f"{responseJson['collection']['permalink']}?ref=0x18090cda49b21deaffc21b4f886aed3eb787d032" if responseJson['collection'].get('permalink') else None,
-            collectionExternalUrl=responseJson['collection']['external_url'],
+            collectionName=responseJson.get('collection', {}).get('name'),
+            collectionImageUrl=responseJson.get('collection', {}).get('large_image_url') or responseJson.get('collection', {}).get('image_url'),
+            collectionOpenSeaUrl=f"{responseJson.get('collection', {}).get('permalink')}?ref=0x18090cda49b21deaffc21b4f886aed3eb787d032" if responseJson.get('collection', {}).get('permalink') else None,
+            collectionExternalUrl=responseJson.get('collection', {}).get('external_url'),
         )
         return registryToken
