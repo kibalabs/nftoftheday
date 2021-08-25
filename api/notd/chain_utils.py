ed15ffe01930cdca0ad7b33141f8b7d0babcb2c7
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-08-25 15:07:53 +0100

    .

--- api/notd/chain_utils.py
+++ api/notd/chain_utils.py
@@ -1,6 +1,6 @@
 import eth_utils
 
 
-def normalize_32_byte_hex_address(value):
-    as_bytes = eth_utils.to_bytes(hexstr=value)
-    return str(eth_utils.to_normalized_address(as_bytes[-20:]))
+def normalize_address(value):
+    address = eth_utils.to_bytes(hexstr=value)
+    return str(eth_utils.to_normalized_address(address[-20:]))
