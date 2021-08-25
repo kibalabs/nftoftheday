b2be5e9a663c862c59c7bee0fb53b8d3f822fee0
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-08-25 20:25:46 +0100

    added typing

--- api/notd/chain_utils.py
+++ api/notd/chain_utils.py
@@ -2,6 +2,6 @@ from eth_typing.encoding import HexStr
 import eth_utils
 
 
-def normalize_address(value:HexStr)->str:
+def normalize_address(value: str ) -> str:
     address = eth_utils.to_bytes(hexstr=value)
     return str(eth_utils.to_normalized_address(address[-20:]))
