94a8611258832663063e00930361ecc97c132410
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-08-31 20:31:02 +0100

    used SqLalchemy

--- api/scrpts/fix_addresses.py
+++ api/scrpts/fix_addresses.py
@@ -1,38 +1,30 @@
+
 import os
-import psycopg2
-from psycopg2 import Error
+import asyncio
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, MetaData
 from notd.chain_utils import normalize_address
 
-try:
-    # Connect to an existing database
-    connection = psycopg2.connect(user=os.environ['DB_USERNAME'],
-                                  password=os.environ['DB_PASSWORD'],
-                                  host=os.environ['DB_HOST'],
-                                  port=os.environ['DB_PORT'],
-                                  database=os.environ['DB_NAME'])
+async def fix_address():
+    db_string = (f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')# pylint: disable=invalid-name
+    db = create_engine(db_string) # pylint: disable=invalid-name
+    meta = MetaData(db) # pylint: disable=invalid-name
+    table = Table('tbl_token_transfers', meta,
+                       Column('to_address', String),
+                       Column('from_address', String),
+                       Column('id',String)) # pylint: disable=invalid-name
+    conn=db.connect()
 
-    cursor = connection.cursor()
-    postgreSQLselectQuery =  "SELECT to_address,from_address,id FROM tbl_token_transfers"  # pylint: disable=invalid-name
-    cursor.execute(postgreSQLselectQuery)
-    db = cursor.fetchall()
-    print("Print each row and it's columns values")
-    for row in db:
-        if len(row[0])>42:
-            fixedAddress="'"+(normalize_address(row[0]))+"'"  # pylint: disable=invalid-name
-            query = 'update tbl_token_transfers set to_address = {} where id = {}'.format(fixedAddress,row[2])  # pylint: disable=invalid-name
-            cursor.execute(query)
-            connection.commit()
-        if len(row[1])>42:
-            fixedAddress="'"+(normalize_address(row[1]))+"'"  # pylint: disable=invalid-name
-            query = 'update tbl_token_transfers set from_address = {} where id = {}'.format(fixedAddress,row[2])  # pylint: disable=invalid-name
-            cursor.execute(query)
-            connection.commit()
-        else:
-            pass
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+    result_set = conn.execute(table.select().order_by(table.c.id) ) # pylint: disable=invalid-name
+    for to_address,from_address,_ in result_set: # pylint: disable=invalid-name
+        if len(to_address)>42:
+            fixedAddress=(normalize_address(to_address))  # pylint: disable=invalid-name
+            update = table.update().where(table.c.to_address==to_address).values(to_address = fixedAddress)
+            conn.execute(update)
+        if len(from_address)>42:
+            fixedAddress=(normalize_address(from_address))  # pylint: disable=invalid-name
+            update = table.update().where(table.c.from_address==from_address).values(from_address = fixedAddress) # pylint: disable=no-value-for-parameter
+            conn.execute(update)
+
+if __name__ == '__main__':
+    asyncio.run(fix_address())
