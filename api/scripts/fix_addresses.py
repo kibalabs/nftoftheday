b2fd463a9a8d2a4fbd08b1372f1fa6a125ba7b92
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-09-01 13:06:39 +0100

    .

--- api/scripts/fix_addresses.py
+++ api/scripts/fix_addresses.py
@@ -1,24 +1,30 @@
 
 import os
 import asyncio
+import asyncclick as click
+
 from sqlalchemy import create_engine
 from notd.chain_utils import normalize_address
 from notd.store.schema import TokenTransfersTable as table
 
-async def fix_address():
-    dbString = (f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')# pylint: disable=invalid-name
+@click.command()
+@click.option('-s', '--start-block-number', 'startBlockNumber', required=True, type=int)
+@click.option('-e', '--end-block-number', 'endBlockNumber', required=True, type=int)
+@click.option('-b', '--batch-size', 'batchSize', required=False, type=int, default=25)
+async def fix_address(startBlockNumber: int, endBlockNumber: int, batchSize: int):
+    dbString = (f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')#pylint: disable=invalid-name
     db = create_engine(dbString) # pylint: disable=invalid-name
 
     conn = db.connect()
 
-    result_set = conn.execute(table.select().order_by(table.c.id) ) # pylint: disable=invalid-name
-    for toAddress,fromAddress,_ in result_set:
+    resultSet = conn.execute(table.select().where(table.c.block_number.between(startBlockNumber,endBlockNumber)).order_by(table.c.id) ) #pylint: disable=invalid-name
+    for toAddress, fromAddress, _ in resultSet:
         if len(toAddress) > 42:
-            fixedAddress = (normalize_address(toAddress))
+            fixedAddress = normalize_address(toAddress)
             update = table.update().where(table.c.to_address == toAddress).values(to_address = fixedAddress)
             conn.execute(update)
         if len(fromAddress) > 42:
-            fixedAddress = (normalize_address(fromAddress))
+            fixedAddress = normalize_address(fromAddress)
             update = table.update().where(table.c.from_address == fromAddress).values(from_address = fixedAddress)
             conn.execute(update)
 
