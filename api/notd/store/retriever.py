afbc22d6e0636dde4837c3d0bb6dc75eb905a338
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-09-14 12:18:35 +0100

    .

--- api/notd/store/retriever.py
+++ api/notd/store/retriever.py
@@ -39,6 +39,15 @@ class Retriever(CoreRetriever):
         async for row in self.database.iterate(query=query):
             tokenTransfer = token_transfer_from_row(row)
             yield tokenTransfer
+        
+    async def get_transaction_count(self, startDate: datetime.datetime, endDate: datetime.datetime) -> int:
+        query = TokenTransfersTable.select()
+        query = query.where(TokenTransfersTable.c.blockDate >= startDate)
+        query = query.where(TokenTransfersTable.c.blockDate < endDate)
+        query = query.where(TokenTransfersTable.c.registryAddress.notin_(_REGISTRY_BLACKLIST))
+        query = query.with_only_columns([sqlalchemyfunc.count()]).order_by(None)
+        count = await self.database.execute(query)
+        return count
 
     async def get_most_traded_token(self, startDate: datetime.datetime, endDate: datetime.datetime) -> Token:
         query = TokenTransfersTable.select()
