57d9f35d01f497e1e20ecbd41568985e6521b24b
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2021-09-15 09:19:28 +0100

    Feature: show loading transactions while the page is loading (#80)
    * show - total transaction on loading
    
    * change initial value to null
    
    * total count update

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -46,7 +46,7 @@ export const App = (): React.ReactElement => {
   const [randomTokenTransfer, setRandomTokenTransfer] = React.useState<TokenTransfer | null>(null);
   const [mostTradedTokenTransfers, setMostTradedTokenTransfers] = React.useState<TokenTransfer[] | null>(null);
   const [sponsoredToken, setSponsoredToken] = React.useState<Token | null>(null);
-  const [transactionCount, setTransactionCount] = React.useState<number | null>(0);
+  const [transactionCount, setTransactionCount] = React.useState<number | null>(null);
 
   const getUrlDate = (key: string): Date | null => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -115,6 +115,7 @@ export const App = (): React.ReactElement => {
     newDate.setDate(newDate.getDate() - 1);
     newDate.setHours(0, 0, 0, 0);
     setStartDate(newDate);
+    setTransactionCount(null);
   };
 
   const onForwardClicked = (): void => {
@@ -122,6 +123,7 @@ export const App = (): React.ReactElement => {
     newDate.setDate(newDate.getDate() + 1);
     newDate.setHours(0, 0, 0, 0);
     setStartDate(newDate);
+    setTransactionCount(null);
   };
 
   const onEmailClicked = (): void => {
@@ -169,7 +171,11 @@ export const App = (): React.ReactElement => {
               <IconButton icon={<KibaIcon iconId='ion-chevron-forward' />} onClicked={onForwardClicked} isEnabled={startDate < defaultDate} />
             </Stack>
             <Spacing variant={PaddingSize.Wide2} />
-            <Text variant='header3'>{`${numberWithCommas(transactionCount)} transactions in total`}</Text>
+            { transactionCount !== null ? (
+              <Text variant='header3'>{`${numberWithCommas(transactionCount)} transactions in total`}</Text>
+            ) : (
+              <Text variant='header3'>Loading transactions...</Text>
+            )}
             <Spacing variant={PaddingSize.Default} />
             <Stack.Item growthFactor={1} shrinkFactor={1}>
               <Spacing variant={PaddingSize.Wide2} />
