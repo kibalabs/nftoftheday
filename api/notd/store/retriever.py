4bab4d2ec1a438f80c800f91dac53ba9ddcd351b
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-09-15 14:47:19 +0100

    .

--- api/notd/store/retriever.py
+++ api/notd/store/retriever.py
@@ -7,7 +7,7 @@ from core.store.retriever import FieldFilter
 from core.store.retriever import Order
 from core.store.retriever import Retriever as CoreRetriever
 from core.store.retriever import StringFieldFilter
-from sqlalchemy.sql.expression import func as sqlalchemyfunc, or_
+from sqlalchemy.sql.expression import func as sqlalchemyfunc
 
 from notd.model import Token
 from notd.model import TokenTransfer
@@ -20,6 +20,7 @@ _REGISTRY_BLACKLIST = set([
     '0xC36442b4a4522E871399CD717aBDD847Ab11FE88', # uniswap-v3-positions
     '0xb9ed94c6d594b2517c4296e24a8c517ff133fb6d', # hegic-eth-atm-calls-pool
 ])
+
 class Retriever(CoreRetriever):
 
     async def list_token_transfers(self, fieldFilters: Optional[Sequence[FieldFilter]] = None, orders: Optional[Sequence[Order]] = None, limit: Optional[int] = None) -> Sequence[TokenTransfer]:
@@ -38,7 +39,6 @@ class Retriever(CoreRetriever):
         async for row in self.database.iterate(query=query):
             tokenTransfer = token_transfer_from_row(row)
             yield tokenTransfer
-
 
     async def get_transaction_count(self, startDate: datetime.datetime, endDate: datetime.datetime) -> int:
         query = TokenTransfersTable.select()
