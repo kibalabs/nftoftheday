6ba194234a15d4d3c8ab5b7523cca19be425e651
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-09-16 20:37:05 +0100

    .

--- api/scripts/owned_tokens.py
+++ api/scripts/owned_tokens.py
@@ -6,51 +6,45 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 import asyncio
 import logging
-import collections
 
 import asyncclick as click
 from databases.core import Database
-from sqlalchemy.sql.expression import or_
 from notd.store.retriever import Retriever
 from notd.store.schema import TokenTransfersTable
 from notd.store.schema_conversions import token_transfer_from_row
 
 @click.command()
-@click.option('-a', '--owner-address', 'ownerAddress', required=False, type=str)
-async def ownedTokens(ownerAddress: Optional[str]):
+@click.option('-o', '--owner-address', 'ownerAddress', required=False, type=str)
+async def owned_tokens(ownerAddress: Optional[str]):
     database = Database(f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')
     retriever = Retriever(database=database)
-    await database.connect()
 
-    ownerAddress = '0x63A2368F4B509438ca90186cb1C15156713D5834'
-   
-    #boughtTokens = {}
-    boughtTokens = collections.defaultdict(list)
-    soldTokens= {}
+    await database.connect()
+    boughtTokens = []
+    soldTokens= []
     async with database.transaction():
         query = TokenTransfersTable.select()
         query = query.where(TokenTransfersTable.c.toAddress == ownerAddress)
         async for row in retriever.database.iterate(query=query):
             tokenTransfer = token_transfer_from_row(row)
-            boughtTokens[tokenTransfer.registryAddress].append(tokenTransfer.tokenId)
+            boughtTokens.append(tokenTransfer)
         
         query = TokenTransfersTable.select()
         query = query.where(TokenTransfersTable.c.fromAddress == ownerAddress)
         async for row in retriever.database.iterate(query=query):
             tokenTransfer = token_transfer_from_row(row)
-            soldTokens[tokenTransfer.registryAddress] = tokenTransfer.tokenId
-       
-        print(boughtTokens)
-        print(soldTokens)
+            soldTokens.append(tokenTransfer)
 
-        uniqueBoughtTokens = len(boughtTokens.values())
-        uniqueSoldTokens = len(soldTokens.values())
+        uniqueBoughtTokens = set(boughtTokens)
+        uniqueSoldTokens = set(soldTokens)
         tokensOwned = uniqueBoughtTokens - uniqueSoldTokens
-        print(tokensOwned)
+
+        for tokenTransfer in tokensOwned:
+            logging.info(f'Tokens Owned: registry_address: {tokenTransfer.registryAddress}, token_id: {tokenTransfer.tokenId}')
 
     await database.disconnect()
     logging.info(f'Got {len(tokensOwned)} total owned')    
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(ownedTokens())
+    asyncio.run(owned_tokens())
