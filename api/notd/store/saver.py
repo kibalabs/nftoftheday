3ea50b1bbb0a0a5547c18f8a32de4232a8b17ada
Obafemi OGUNKOLA <31977729+Femi-Ogunkola@users.noreply.github.com>
2021-10-15 16:21:08 +0100

    Update api/notd/store/saver.py
    Co-authored-by: Krishan Patel <krishan711@gmail.com>

--- api/notd/store/saver.py
+++ api/notd/store/saver.py
@@ -86,7 +86,7 @@ class Saver(CoreSaver):
             attributes=attributes,
         )
 
-    async def update_token_metadata_item(self, registryAddress: str, tokenId: str, metadataUrl: Optional[str] = None, description: Optional[str] = _EMPTY_STRING, imageUrl: Optional[str] = _EMPTY_STRING, name: Optional[str] = _EMPTY_STRING, attributes: Optional[str] = _EMPTY_OBJECT) -> None:
+    async def update_token_metadata(self, registryAddress: str, tokenId: str, metadataUrl: Optional[str] = None, description: Optional[str] = _EMPTY_STRING, imageUrl: Optional[str] = _EMPTY_STRING, name: Optional[str] = _EMPTY_STRING, attributes: Optional[str] = _EMPTY_OBJECT) -> None:
         query = TokenMetadataTable.update(and_(TokenMetadataTable.c.registryAddress == registryAddress,TokenMetadataTable.c.tokenId ==tokenId))
         values = {}
         if metadataUrl is not None:
