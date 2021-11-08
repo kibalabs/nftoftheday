3fd0d9e416e98d898e1ac51bfc848d05dd716e59
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-11-08 21:59:03 +0100

    .

--- api/notd/collection_processor.py
+++ api/notd/collection_processor.py
@@ -47,7 +47,7 @@ class CollectionProcessor:
                 if exception.statusCode == 429:
                     raise
                 if exception.statusCode >= 500 and retryCount >= 3:
-                    raise CollectionDoesNotExist()  
+                    raise CollectionDoesNotExist()
                 logging.info(f'Retrying due to: {str(exception)}')
                 await asyncio.sleep(1.5)
                 retryCount += 1
