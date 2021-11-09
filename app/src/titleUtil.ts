5688dbdcb02b5020dc03355c789ee0a72f61a4b9
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2021-11-09 12:00:01 +0100

    add return

--- app/src/titleUtil.ts
+++ app/src/titleUtil.ts
@@ -1,3 +1,4 @@
-export const truncateTitle = (title: string, maxLength = 50): string => (title.length > maxLength
-  ? `${title.substring(0, maxLength)}…`
-  : title);
+export const truncateTitle = (title: string, maxLength = 50): string => {
+  return (
+    title.length > maxLength ? `${title.substring(0, maxLength)}…` : title);
+};
