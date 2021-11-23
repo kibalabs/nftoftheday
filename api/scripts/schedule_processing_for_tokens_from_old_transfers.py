1bcd864887754cd824474ca367efbd0bff417c39
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-11-23 19:37:23 +0100

    .

--- api/scripts/schedule_processing_for_tokens_from_old_transfers.py
+++ api/scripts/schedule_processing_for_tokens_from_old_transfers.py
@@ -24,10 +24,8 @@ from notd.store.schema_conversions import token_transfer_from_row
 @click.option('-b', '--batch-size', 'batchSize', required=False, type=int, default=100)
 async def add_message(startBlockNumber: int, endBlockNumber: int, batchSize: int):
     database = Database(f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')
-    #sqsClient = boto3.client(service_name='sqs', region_name='eu-west-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
-    #workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-work-queue')
-    sqsClient = boto3.client(service_name='sqs', region_name='us-east-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
-    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.us-east-1.amazonaws.com/113848722427/test1')
+    sqsClient = boto3.client(service_name='sqs', region_name='eu-west-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
+    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-work-queue')
     await database.connect()
     cache = set()
     registryCache = set()
@@ -41,27 +39,16 @@ async def add_message(startBlockNumber: int, endBlockNumber: int, batchSize: int
             query = TokenTransfersTable.select()
             query = query.where(TokenTransfersTable.c.blockNumber >= start)
             query = query.where(TokenTransfersTable.c.blockNumber < end)
-            query = query.where(TokenTransfersTable.c.registryAddress.in_(
-                TokenTransfersTable.select()
-                .with_only_columns([TokenTransfersTable.c.registryAddress])
-                    .group_by(TokenTransfersTable.c.registryAddress)
-            ))
             tokenTransfersToChange = [token_transfer_from_row(row) async for row in database.iterate(query=query)]
             for tokenTransfer in tokenTransfersToChange:
                 if (tokenTransfer.registryAddress, tokenTransfer.tokenId) in cache:
-                    pass
-                else:
-                    cache.add((tokenTransfer.registryAddress,tokenTransfer.tokenId))
-                    await workQueue.send_message(message=UpdateTokenMetadataMessageContent(registryAddress=tokenTransfer.registryAddress, tokenId=tokenTransfer.tokenId).to_message())
-                    if tokenTransfer.registryAddress in registryCache:
-                        pass
-                    else:
-                        await workQueue.send_message(message=UpdateCollectionMessageContent(address=tokenTransfer.registryAddress).to_message())
-
-        logging.info(f'Found {len(cache)} unique tokens between {startBlockNumber} and {end}')
+                    continue
+                cache.add((tokenTransfer.registryAddress,tokenTransfer.tokenId))
+                await workQueue.send_message(message=UpdateTokenMetadataMessageContent(registryAddress=tokenTransfer.registryAddress, tokenId=tokenTransfer.tokenId).to_message())
+                if tokenTransfer.registryAddress in registryCache:
+                    continue
+                await workQueue.send_message(message=UpdateCollectionMessageContent(address=tokenTransfer.registryAddress).to_message())
         currentBlockNumber = currentBlockNumber + batchSize
-    
-
     await database.disconnect()
 
 if __name__ == '__main__':
