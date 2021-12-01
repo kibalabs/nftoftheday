931898942383f67b2d205c5aa98eab8e2edf46d6
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-12-01 22:43:09 +0100

    .

--- api/scripts/check_all_blocks_are_processed.py
+++ api/scripts/check_all_blocks_are_processed.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+import boto3
+from core.queues.sqs_message_queue import SqsMessageQueue
+
+from api.notd.messages import ProcessBlocksMessageContent
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 import asyncio
@@ -14,27 +19,23 @@ from notd.store.schema import TokenTransfersTable
 
 
 @click.command()
-@click.option('-s', '--start-block-number', 'startBlockNumber', required=True, type=int)
-@click.option('-e', '--end-block-number', 'endBlockNumber', required=True, type=int)
-@click.option('-b', '--batch-size', 'batchSize', required=False, type=int, default=100)
-async def check_all_processed(startBlockNumber: int, endBlockNumber: int, batchSize: int):
+async def check_all_processed():
     database = Database(f'postgresql://{os.environ["REMOTE_DB_USERNAME"]}:{os.environ["REMOTE_DB_PASSWORD"]}@{os.environ["REMOTE_DB_HOST"]}:{os.environ["REMOTE_DB_PORT"]}/{os.environ["REMOTE_DB_NAME"]}')
     #database = Database(f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')#pylint: disable=invalid-name
+    sqsClient = boto3.client(service_name='sqs', region_name='eu-west-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
+    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-work-queue')
     blocks_processed = []
     await database.connect()
-    query = TokenTransfersTable.select().with_only_columns([TokenTransfersTable.c.blockNumber])
-    query = query.where(TokenTransfersTable.c.blockNumber.in_(
-        TokenTransfersTable.select().with_only_columns([TokenTransfersTable.c.blockNumber]).group_by(TokenTransfersTable.c.blockNumber)
-    ))
+    query = TokenTransfersTable.select().with_only_columns([TokenTransfersTable.c.blockNumber]).distinct(TokenTransfersTable.c.blockNumber)
     rows = await database.fetch_all(query)
     for row in rows:
         blocks_processed.append(row[0])
     await database.disconnect()
+
     print(f'Checking for x')
-    for i in range(3919706,13708119):
-        if i not in blocks_processed:
-            print(i)
-    
+    notProcessed = set(range(3919706,13708119)) - set(blocks_processed)
+    await workQueue.send_message(message=ProcessBlocksMessageContent(blockNumbers=notProcessed).to_message())
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(check_all_processed())
