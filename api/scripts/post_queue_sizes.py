03059e6d52472515a04cd04b27d93fadb466bf2c
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-12-11 03:52:15 +0100

    .

--- api/scripts/post_queue_sizes.py
+++ api/scripts/post_queue_sizes.py
@@ -8,13 +8,13 @@ from core.requester import Requester
 from core.slack_client import SlackClient
 
 async def post():
-    sqsClient = boto3.client(service_name='sqs', region_name='us-east-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
-    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.us-east-1.amazonaws.com/113848722427/test1')
+    sqsClient = boto3.client(service_name='sqs', region_name='eu-west-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
+    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-work-queue')
     requester = Requester()
     slackClient = SlackClient(webhookUrl=os.environ['SLACK_WEBHOOK_URL'], requester=requester, defaultSender='worker', defaultChannel='notd-notifications')
+
     queuesResponse = await workQueue.get_queues()
     queueUrls = queuesResponse.get("QueueUrls")
-
     messages =[]
     for queueUrl in queueUrls:
         len = await workQueue.get_queue_attributes(queue=queueUrl)
