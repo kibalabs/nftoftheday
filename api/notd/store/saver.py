e8b8fa1655417ffd69aa0f720969d7250bc4dfa2
Krishan Patel <krishan711@gmail.com>
2022-01-10 20:56:34 +0000

    .

--- api/notd/store/saver.py
+++ api/notd/store/saver.py
@@ -35,7 +35,6 @@ class Saver(CoreSaver):
             await transaction.commit()
 
     async def create_token_transfer(self, retrievedTokenTransfer: RetrievedTokenTransfer) -> TokenTransfer:
-        print('retrievedTokenTransfer', retrievedTokenTransfer)
         tokenTransferId = await self._execute(query=TokenTransfersTable.insert(), values={  # pylint: disable=no-value-for-parameter
             TokenTransfersTable.c.transactionHash.key: retrievedTokenTransfer.transactionHash,
             TokenTransfersTable.c.registryAddress.key: retrievedTokenTransfer.registryAddress,
