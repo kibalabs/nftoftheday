297d0467f2287acfec305457d170bdef7a2e8299
Krishan Patel <krishan711@gmail.com>
2022-01-24 00:14:31 +0000

    .

--- dbschema/1.tables.sql
+++ dbschema/1.tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE tbl_token_transfers (
     token_id TEXT NOT NULL,
     value NUMERIC(256, 0) NOT NULL,
     -- NOTE(krishan711): amount2 should be changed to amount and NOT NULL once filled
-    amount2 NUMERIC(256, 0),
+    amount_2 NUMERIC(256, 0),
     gas_limit NUMERIC(256, 0) NOT NULL,
     gas_price NUMERIC(256, 0) NOT NULL,
     gas_used NUMERIC(256, 0) NOT NULL,
