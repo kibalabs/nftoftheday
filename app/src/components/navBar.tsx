b9618a41c9a8febb305d5812ab142020f7b62d29
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-01-24 12:34:25 +0100

    nav bar

--- app/src/components/navBar.tsx
+++ app/src/components/navBar.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import { Alignment, Direction, Stack } from '@kibalabs/ui-react';
+import { Alignment, Box, Button, Direction, Image, PaddingSize, Stack, Text } from '@kibalabs/ui-react';
 
 export const NavBar = (): React.ReactElement => {
   return (
-    <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} >
-      
-    </Stack>
+    <Box height='' isFullWidth={true}>
+      <Stack direction={Direction.Horizontal} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} shouldAddGutters={true} padding={PaddingSize.Wide1}>
+        <Box variant='card' shouldClipContent={true} width='50px' height='50px'>
+          <Image source='assets/icon.png' alternativeText='logo' fitType='contain' />
+        </Box>
+        <Text variant='title'>Token Hunt</Text>
+        <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.End} shouldAddGutters={true} padding={PaddingSize.Wide1}>
+          <Button variant='secondary' text= 'Connect Wallet' />
+          <Box variant='rounded-borderColored' shouldClipContent={true} height='30px' width='30px'>
+            <Image source='assets/icon.png' alternativeText='profile picture' />
+          </Box>
+          <Text>0x876...988765</Text>
+        </Stack>
+
+      </Stack>
+    </Box>
   );
 };
