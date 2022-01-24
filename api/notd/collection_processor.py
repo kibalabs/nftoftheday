5da2d4d8ddb8192ece4f696e56de4063834eddcc
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2022-01-24 17:21:45 +0100

    .

--- api/notd/collection_processor.py
+++ api/notd/collection_processor.py
@@ -26,6 +26,7 @@ class CollectionProcessor:
         self.erc721MetdataContractAbi = erc721MetdataContractJson['abi']
         self.erc721MetadataNameFunctionAbi = [internalAbi for internalAbi in self.erc721MetdataContractAbi if internalAbi['name'] == 'name'][0]
         self.erc721MetadataSymbolFunctionAbi = [internalAbi for internalAbi in self.erc721MetdataContractAbi if internalAbi['name'] == 'symbol'][0]
+        self.erc721MetdataContractURI =  [internalAbi for internalAbi in self.erc721MetdataContractAbi if internalAbi['name'] == 'contractURI'][0]
 
         with open('./contracts/contractABI.json') as contractJsonFile:
             contractJson = json.load(contractJsonFile)
@@ -33,6 +34,11 @@ class CollectionProcessor:
         self.contractUri = [internalAbi for internalAbi in self.contractAbi[1:] if internalAbi['name'] == 'contractURI'][0]
 
     async def retrieve_collection(self, address: str) -> RetrievedCollection:
+        try:
+            contractUriResponse = await self.ethClient.call_function(toAddress=address, contractAbi=self.erc721MetdataContractAbi, functionAbi=self.erc721MetdataContractURI)
+            contractMetadataUri = contractUriResponse[0]
+        except BadRequestException:
+            contractMetadataUri = None
         try:
             tokenMetadataNameResponse = await self.ethClient.call_function(toAddress=address, contractAbi=self.erc721MetdataContractAbi, functionAbi=self.erc721MetadataNameFunctionAbi)
             collectionName = tokenMetadataNameResponse[0]
@@ -43,34 +49,35 @@ class CollectionProcessor:
             collectionSymbol = tokenMetadataSymbolResponse[0]
         except BadRequestException:
             collectionSymbol = None
-        try:
-            contractUriResponse = await self.ethClient.call_function(toAddress=address, contractAbi=self.contractAbi, functionAbi=self.contractUri)
-            contract = contractUriResponse[0]
-            print(contract)
-        except BadRequestException:
-            collectionSymbol = None
-        openseaResponse = None
-        retryCount = 0
-        while not openseaResponse:
-            try:
-                openseaResponse = await self.requester.get(url=f'https://api.opensea.io/api/v1/asset_contract/{address}', headers={"X-API-KEY": self.openseaApiKey})
-            except ResponseException as exception:
-                if exception.statusCode == 404:
-                    raise CollectionDoesNotExist()
-                if retryCount >= 3 or (exception.statusCode < 500 and exception.statusCode != 429):
-                    break
-                logging.info(f'Retrying due to: {str(exception)}')
-                await asyncio.sleep(1.5)
-            except ReadTimeout as exception:
-                if retryCount >= 3:
-                    break
-                logging.info(f'Retrying due to: {str(exception)}')
-                await asyncio.sleep(1.5)
-            retryCount += 1
-        openseaCollection = openseaResponse.json().get('collection') if openseaResponse else {}
-        if openseaCollection is None:
-            logging.info(f'Failed to load collection from opensea: {address}')
-            openseaCollection = {}
+        if contractMetadataUri is not None:
+            print(contractMetadataUri)
+            contractMetadataUriResponse = await self.requester.get(url=contractMetadataUri)
+            openseaCollection = contractMetadataUriResponse.json()
+            if isinstance(openseaCollection, str):
+                    openseaCollection = json.loads(openseaCollection)
+        else:
+            openseaResponse = None
+            retryCount = 0
+            while not openseaResponse:
+                try:
+                    openseaResponse = await self.requester.get(url=f'https://api.opensea.io/api/v1/asset_contract/{address}', headers={"X-API-KEY": self.openseaApiKey})
+                except ResponseException as exception:
+                    if exception.statusCode == 404:
+                        raise CollectionDoesNotExist()
+                    if retryCount >= 3 or (exception.statusCode < 500 and exception.statusCode != 429):
+                        break
+                    logging.info(f'Retrying due to: {str(exception)}')
+                    await asyncio.sleep(1.5)
+                except ReadTimeout as exception:
+                    if retryCount >= 3:
+                        break
+                    logging.info(f'Retrying due to: {str(exception)}')
+                    await asyncio.sleep(1.5)
+                retryCount += 1
+            openseaCollection = openseaResponse.json().get('collection') if openseaResponse else {}
+            if openseaCollection is None:
+                logging.info(f'Failed to load collection from opensea: {address}')
+                openseaCollection = {}
         retrievedCollection = RetrievedCollection(
             address=address,
             name=collectionName or openseaCollection.get('name'),
@@ -81,9 +88,8 @@ class CollectionProcessor:
             instagramUsername=openseaCollection.get('instagram_username'),
             wikiUrl=openseaCollection.get('wiki_url'),
             openseaSlug=openseaCollection.get('slug'),
-            url=openseaCollection.get('external_url'),
+            url=openseaCollection.get('external_url') or contractMetadataUri,
             discordUrl=openseaCollection.get('discord_url'),
             bannerImageUrl=openseaCollection.get('banner_image_url'),
         )
-        print(retrievedCollection)
         return retrievedCollection
