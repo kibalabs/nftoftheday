60fb4dd195a99eaaadb95af28236cddfe28e9830
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-01-25 12:40:10 +0100

    add wordmark

--- app/src/components/navBar.tsx
+++ app/src/components/navBar.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-import { Alignment, Box, Button, Direction, Image, PaddingSize, Stack, Text } from '@kibalabs/ui-react';
+import { Alignment, Box, Button, Direction, Image, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
 
 export const NavBar = (): React.ReactElement => {
   return (
-    <Box height='' isFullWidth={true}>
-      <Stack direction={Direction.Horizontal} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} shouldAddGutters={true} padding={PaddingSize.Wide1}>
-        <Box variant='card' shouldClipContent={true} width='50px' height='50px'>
+    <Box height='64px' isFullWidth={true}>
+      <Stack direction={Direction.Horizontal} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} padding={PaddingSize.Wide1}>
+        <Box shouldClipContent={true} width='40px' height='40px'>
           <Image source='assets/icon.png' alternativeText='logo' fitType='contain' />
         </Box>
-        <Text variant='title'>Token Hunt</Text>
-        <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.End} shouldAddGutters={true} padding={PaddingSize.Wide1}>
-          <Button variant='secondary' text= 'Connect Wallet' />
-          <Box variant='rounded-borderColored' shouldClipContent={true} height='30px' width='30px'>
-            <Image source='assets/icon.png' alternativeText='profile picture' />
-          </Box>
-          <Text>0x876...988765</Text>
-        </Stack>
-
+        <Image source='assets/wordmark.svg' alternativeText='watermark' fitType='contain' />
+        <Stack.Item growthFactor={1} shrinkFactor={1}>
+          <Spacing variant={PaddingSize.Wide2} />
+        </Stack.Item>
+        <Button variant='secondary' text= 'Connect Wallet' />
+        <Box variant='rounded-borderColored' shouldClipContent={true} height='20px' width='20px'>
+          <Image source='assets/icon.png' alternativeText='profile picture' />
+        </Box>
+        <Text>0x876...988765</Text>
       </Stack>
     </Box>
   );
