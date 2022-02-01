5b5dd2ce1a90ec18a638c9bafe5ac795cd9565c6
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2022-02-02 00:06:44 +0100

    .

--- api/scripts/download_from_s3.py
+++ api/scripts/download_from_s3.py
@@ -1,43 +1,54 @@
+import datetime
 import os
 import sys
 import asyncio
 import logging
 import boto3
 import json
+from databases import Database
+
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+import asyncclick as click
 
-from core.queues.sqs_message_queue import SqsMessageQueue
-from core.requester import Requester
+from notd.store.schema import TokenCollectionsTable, TokenMetadataTable
+from notd.store.schema_conversions import collection_from_row
+from core.util import date_util
 from core.s3_manager import S3Manager
-from core.web3.eth_client import RestEthClient
-
-from notd.block_processor import BlockProcessor
 
 
-
-
-async def download_from_s3():
-    sqsClient = boto3.client(service_name='sqs', region_name='us-east-1', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
-    workQueue = SqsMessageQueue(sqsClient=sqsClient, queueUrl='https://sqs.us-east-1.amazonaws.com/113848722427/test1')
-    requester = Requester()
+@click.command()
+@click.option('-s', '--start-id-number', 'startId', required=True, type=int)
+@click.option('-e', '--end-id-number', 'endId', required=True, type=int)
+@click.option('-b', '--batch-size', 'batchSize', required=False, type=int, default=100)
+async def download_from_s3(startId: int, endId: int, batchSize: int):
+    database = Database(f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')
     s3Client = boto3.client(service_name='s3', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
     s3manager = S3Manager(s3Client=s3Client)
-    requester = Requester()
-    ethClient = RestEthClient(url=f'https://mainnet.infura.io/v3/{os.environ["INFURA_PROJECT_ID"]}', requester=requester)
-    blockProcessor = BlockProcessor(ethClient=ethClient)
-    requester = Requester()
     bucketName = os.environ['S3_BUCKET']
     
-    set_of_attributes = set()
-    for s3_file in (await s3manager.list_directory_files(s3Directory=f'{bucketName}/token-metadatas/')):
-        a = await s3manager.read_file(sourcePath=f'{s3_file.bucket}/{s3_file.path}')
-        f = json.loads(a)
-        b = set(list(f.keys()))
-        set_of_attributes = set_of_attributes | b
-        print(set_of_attributes)
-
-    print(set_of_attributes)
-    return
+    await database.connect()
+    currentId = startId
+    while currentId < endId:
+        start = currentId
+        end = min(currentId + batchSize, endId)
+        logging.info(f'Working on {start} to {end}...')
+        async with database.transaction():
+            query = TokenCollectionsTable.select()
+            query = query.where(TokenCollectionsTable.c.collectionId >= start)
+            query = query.where(TokenCollectionsTable.c.collectionId < end)
+        collections = [collection_from_row(row) async for row in database.iterate(query=query)]
+        for collection in collections:
+            tokens = []
+            for s3_file in (await s3manager.list_directory_files(s3Directory=f'{bucketName}/token-metadatas/{collection.address}')):
+                path = s3_file.path
+                token = path.split('/')[2]#.replace(" ","T")
+                if token not in tokens:
+                    tokens.append(path) 
+            
+        currentId = currentId + batchSize
+        
+    await database.disconnect()
 
 
 if __name__ == '__main__':
