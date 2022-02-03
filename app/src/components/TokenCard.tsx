0beeee8e3f12725e410d135748421a87454322ec
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-02-03 12:08:05 +0100

    Fix padding

--- app/src/components/TokenCard.tsx
+++ app/src/components/TokenCard.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import { Alignment, Box, Direction, Image, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
+import { Alignment, Box, Direction, Image, PaddingSize, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 import { CollectionToken } from '../client/resources';
 
@@ -11,14 +11,16 @@ export interface TokenCardProps {
 
 export const TokenCard = (props:TokenCardProps): React.ReactElement => {
   return (
-    <Box variant='tokenCard' shouldClipContent={true}>
-      <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start}>
+    <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} paddingHorizontal={PaddingSize.Wide1}>
+      <Box variant='tokenCard' shouldClipContent={true}>
         <Box>
           <Image source={props.collectionToken.imageUrl as string} alternativeText='image' fitType='contain' />
         </Box>
-        <Text variant='tokenTitle' alignment={TextAlignment.Center}>{props.collectionToken.name}</Text>
-        <Text alignment={TextAlignment.Center}>sold 12/12/21 for 0.3</Text>
-      </Stack>
-    </Box>
+        <Stack direction={Direction.Vertical} padding={PaddingSize.Wide1}>
+          <Text variant='tokenTitle' alignment={TextAlignment.Center}>{props.collectionToken.name}</Text>
+          <Text alignment={TextAlignment.Center}>sold 12/12/21 for 0.3</Text>
+        </Stack>
+      </Box>
+    </Stack>
   );
 };
