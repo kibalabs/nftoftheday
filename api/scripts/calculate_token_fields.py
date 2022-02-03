61125bef705c39cc07662026d0a89da9f4c306f2
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2022-02-03 19:39:40 +0100

    .

--- api/scripts/calculate_token_fields.py
+++ api/scripts/calculate_token_fields.py
@@ -19,21 +19,20 @@ from core.s3_manager import S3Manager
 
 
 @click.command()
-@click.option('-s', '--start-id-number', 'startId', required=True, type=int)
-@click.option('-e', '--end-id-number', 'endId', required=True, type=int)
+@click.option('-s', '--start-collection-id', 'collectionStartId', required=True, type=int)
+@click.option('-e', '--end-collection-id', 'collectionEndId', required=True, type=int)
 @click.option('-b', '--batch-size', 'batchSize', required=False, type=int, default=100)
-async def download_from_s3(startId: int, endId: int, batchSize: int):
+async def calculate_token_fields(collectionStartId: int, collectionEndId: int, batchSize: int):
     database = Database(f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}')
     s3Client = boto3.client(service_name='s3', aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET'])
     s3manager = S3Manager(s3Client=s3Client)
     bucketName = os.environ['S3_BUCKET']
-    print(bucketName)
     
     await database.connect()
-    currentId = startId
-    while currentId < endId:
+    currentId = collectionStartId
+    while currentId < collectionEndId:
         start = currentId
-        end = min(currentId + batchSize, endId)
+        end = min(currentId + batchSize, collectionEndId)
         logging.info(f'Working on {start} to {end}...')
         query = TokenCollectionsTable.select()
         query = query.where(TokenCollectionsTable.c.collectionId >= start)
@@ -49,13 +48,13 @@ async def download_from_s3(startId: int, endId: int, batchSize: int):
                     break
                 tokenDict = json.loads(await s3manager.read_file(sourcePath=f'{tokenFile.bucket}/{tokenFile.path}'))
                 tokenDict['attributes'] = ",".join(list(set(key for attribute in tokenDict.get('attributes', []) for key in attribute.keys())))
+                tokenDict['description'] = tokenDict.get('description',[])[:10]
                 tokenDict['collection'] = collection.address
-                print(tokenDict)
                 fields.update(tokenDict.keys())
                 rows.append(tokenDict)
-        print(rows)
         with open('./output.tsv', 'w') as outFile:
             dictWriter = csv.DictWriter(outFile, fields, delimiter='\t')
+            print(rows)
             dictWriter.writerows(rows)
         
         currentId = currentId + batchSize
@@ -65,4 +64,4 @@ async def download_from_s3(startId: int, endId: int, batchSize: int):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(download_from_s3())
+    asyncio.run(calculate_token_fields())
