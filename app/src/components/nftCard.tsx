299c667a3842a44bf734c4fcf74429336ae41daf
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-02-07 23:34:19 +0100

    KIBA-1346: Display meebit as Image

--- app/src/components/nftCard.tsx
+++ app/src/components/nftCard.tsx
@@ -84,6 +84,8 @@ export const NftCard = (props: NftCardProps): React.ReactElement => {
                   <div dangerouslySetInnerHTML={{ __html: imageUrl.slice('data:image/svg+xml;utf8,'.length) }} />
                 ) : imageUrl.startsWith('data:image/svg+xml;base64,') ? (
                   <div dangerouslySetInnerHTML={{ __html: atob(imageUrl.slice('data:image/svg+xml;base64,'.length)) }} />
+                ) : imageUrl.includes('meebit') ? (
+                  <Image source={imageUrl} alternativeText={`${asset.name} image`} fitType='contain' />
                 ) : (
                   <Media source={imageUrl} alternativeText={`${asset.name} image`} fitType='contain' />
                 )}
