b50eb389e7689279ae025fabdeeaf7932585b78d
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-02-08 10:41:51 +0100

    .

--- app/src/pages/TokenPage/TokenPage.tsx
+++ app/src/pages/TokenPage/TokenPage.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { Alignment, Direction, Stack } from '@kibalabs/ui-react';
+import { useRouteParams } from '@kibalabs/core-react';
+import { Alignment, Direction, Stack, Text } from '@kibalabs/ui-react';
 
 
 export const TokenPage = (): React.ReactElement => {
-//   const routeParams = useRouteParams();
-//   const address = routeParams.address as string;
-//   const tokenId = routeParams.address as string;
+  const routeParams = useRouteParams();
+  const registryAddress = routeParams.registryAddress as string;
+  const tokenId = routeParams.tokenId as string;
 
   return (
-    <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} isScrollableVertically={true} />
+    <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} isScrollableVertically={true}>
+      <Text variant='header3'>{`Token Page for ${registryAddress} ${tokenId}`}</Text>
+    </Stack>
   );
 };
