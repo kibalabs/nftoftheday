b413b5741768e910e4c3abc2e637ce35145b5ace
Krishan Patel <krishan711@gmail.com>
2022-02-10 18:45:42 +0000

    Feature: implement ResponseBuilder (#228)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    Co-authored-by: Obafemi Ogunkola <ogunkolaobafemi@outlook.com>

---
+++ api/notd/api/endpoints_v1.py
@@ -0,0 +1,50 @@
+
+
+import datetime
+from typing import List
+from typing import Optional
+
+from pydantic import BaseModel
+
+from notd.api.models_v1 import ApiCollection
+from notd.api.models_v1 import ApiCollectionToken
+from notd.api.models_v1 import ApiTokenTransfer
+from notd.api.models_v1 import ApiUiData
+
+
+class RetrieveUiDataRequest(BaseModel):
+    startDate: Optional[datetime.datetime]
+    endDate: Optional[datetime.datetime]
+
+class RetrieveUiDataResponse(BaseModel):
+    uiData: ApiUiData
+
+class ReceiveNewBlocksDeferredRequest(BaseModel):
+    pass
+
+class ReceiveNewBlocksDeferredResponse(BaseModel):
+    pass
+
+class SubscribeRequest(BaseModel):
+    email: str
+
+class SubscribeResponse(BaseModel):
+    pass
+
+class RetrieveCollectionTokenRequest(BaseModel):
+    pass
+
+class RetrieveCollectionTokenResponse(BaseModel):
+    token: ApiCollectionToken
+
+class GetCollectionRecentSalesRequest(BaseModel):
+    pass
+
+class GetCollectionRecentSalesResponse(BaseModel):
+    tokenTransfers: List[ApiTokenTransfer]
+
+class RetrieveCollectionRequest(BaseModel):
+    address: str
+
+class RetrieveCollectionResponse(BaseModel):
+    collection: ApiCollection
