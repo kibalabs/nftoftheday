5c2f12f6e9d5a5b2cdd1539390f3268cf4515387
Krishan Patel <krishan711@gmail.com>
2022-02-11 16:32:58 +0000

    KIBA-1339: Send collection and token processing messages in batches  (#208)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- api/notd/manager.py
+++ api/notd/manager.py
@@ -139,8 +139,12 @@ class NotdManager:
     async def process_block(self, blockNumber: int) -> None:
         retrievedTokenTransfers = await self.blockProcessor.get_transfers_in_block(blockNumber=blockNumber)
         logging.info(f'Found {len(retrievedTokenTransfers)} token transfers in block #{blockNumber}')
-        await asyncio.gather(*[self.update_token_metadata_deferred(registryAddress=retrievedTokenTransfer.registryAddress, tokenId=retrievedTokenTransfer.tokenId) for retrievedTokenTransfer in retrievedTokenTransfers])
-        await asyncio.gather(*[self.update_collection_deferred(address=address) for address in set(retrievedTokenTransfer.registryAddress for retrievedTokenTransfer in retrievedTokenTransfers)])
+        collectionAddresses = list(set(retrievedTokenTransfer.registryAddress for retrievedTokenTransfer in retrievedTokenTransfers))
+        logging.info(f'Found {len(collectionAddresses)} collections in block #{blockNumber}')
+        collectionTokenIds = list(set((retrievedTokenTransfer.registryAddress, retrievedTokenTransfer.tokenId) for retrievedTokenTransfer in retrievedTokenTransfers))
+        logging.info(f'Found {len(collectionTokenIds)} tokens in block #{blockNumber}')
+        await self.tokenManager.update_collections_deferred(addresses=collectionAddresses)
+        await self.tokenManager.update_token_metadatas_deferred(collectionTokenIds=collectionTokenIds)
         await self._save_block_transfers(blockNumber=blockNumber, retrievedTokenTransfers=retrievedTokenTransfers)
 
     @staticmethod
