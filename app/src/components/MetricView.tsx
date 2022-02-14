3143630bcbac7249d152a9294dae80e89294770a
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-02-14 11:52:23 +0100

    KIBA-1350: Create title area on TokenPage (#227)
    * add token title
    
    * change
    
    * account
    
    * fix
    
    * fix
    
    * add collectionView
    
    * fix
    
    * add account
    
    * title and attributes
    
    * Token title
    
    * spacing
    
    * fix
    
    Co-authored-by: Barakat Ajadi <barakat.a.ajadi@gmail.com>

--- app/src/components/MetricView.tsx
+++ app/src/components/MetricView.tsx
@@ -11,8 +11,8 @@ export const MetricView = (props: MetricViewProps): React.ReactElement => {
   return (
     <Box variant='metricCard' height='5rem' width='7rem'>
       <Stack direction={Direction.Vertical} isFullHeight={true} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} padding={PaddingSize.Default}>
-        <Text variant='bold-large'>{props.value}</Text>
-        <Text variant='small' alignment={TextAlignment.Center}>{props.name}</Text>
+        <Text alignment={TextAlignment.Center} variant='bold'>{props.value}</Text>
+        <Text alignment={TextAlignment.Center} variant='small'>{props.name}</Text>
       </Stack>
     </Box>
   );
