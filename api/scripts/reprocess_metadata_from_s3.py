531c2a4c9c4c051bbb3e9f92b6f43a4f86c53b16
Femi <ogunkolaobafemi@gmail.com>
2022-02-15 12:08:09 +0100

    .

--- api/scripts/reprocess_metadata_from_s3.py
+++ api/scripts/reprocess_metadata_from_s3.py
@@ -23,6 +23,7 @@ from notd.token_metadata_processor import TokenMetadataProcessor
 @click.option('-s', '--start-id-number', 'startId', required=False, type=int)
 @click.option('-e', '--end-id-number', 'endId', required=False, type=int)
 async def reprocess_metadata(startId: Optional[int], endId: Optional[int]):
+
     databaseConnectionString = Database.create_psql_connection_string(username=os.environ["DB_USERNAME"], password=os.environ["DB_PASSWORD"], host=os.environ["DB_HOST"], port=os.environ["DB_PORT"], name=os.environ["DB_NAME"])
     database = Database(connectionString=databaseConnectionString)
     saver = Saver(database=database)
@@ -47,12 +48,15 @@ async def reprocess_metadata(startId: Optional[int], endId: Optional[int]):
         tokenMetadataFiles = [file async for file in s3manager.generate_directory_files(s3Directory=tokenDirectory)]
         if len(tokenMetadataFiles) > 0:
             tokenFile=(tokenMetadataFiles[len(tokenMetadataFiles)-1])
-        if tokenFile:
-            tokenMetadataJson = await s3manager.read_file(sourcePath=f'{tokenFile.bucket}/{tokenFile.path}')
-            tokenMetadataDict = json.loads(tokenMetadataJson)
-            retrievedTokenMetadata = await tokenMetadataProcessor._get_token_metadata_from_data(registryAddress=tokenMetadata.registryAddress, tokenId=tokenMetadata.tokenId, metadataUrl=tokenMetadata.metadataUrl, tokenMetadataDict=tokenMetadataDict)
-            logging.info(f'Updating {(tokenMetadata.tokenMetadataId)}')
-            await tokenManger.save_token_metadata(retrievedTokenMetadata=retrievedTokenMetadata)
+            if tokenFile:
+                tokenMetadataJson = await s3manager.read_file(sourcePath=f'{tokenFile.bucket}/{tokenFile.path}')
+                tokenMetadataDict = json.loads(tokenMetadataJson)
+                retrievedTokenMetadata = await tokenMetadataProcessor._get_token_metadata_from_data(registryAddress=tokenMetadata.registryAddress, tokenId=tokenMetadata.tokenId, metadataUrl=tokenMetadata.metadataUrl, tokenMetadataDict=tokenMetadataDict)
+                logging.info(f'Updating {(tokenMetadata.tokenMetadataId)}')
+                await tokenManger.save_token_metadata(retrievedTokenMetadata=retrievedTokenMetadata)
+        else:
+            logging.info(f'Saving Token Metadata for {tokenMetadata.tokenMetadataId}')
+            await tokenManger.update_token_metadata(registryAddress=tokenMetadata.registryAddress, tokenId=tokenMetadata.tokenId)
 
     await database.disconnect()
 
