37729e89bea0fb35ec5bce46c04f26cdab7bbbb7
Femi <ogunkolaobafemi@gmail.com>
2022-02-25 21:04:26 +0100

    .

--- api/scripts/update_collection_tokens.py
+++ api/scripts/update_collection_tokens.py
@@ -14,7 +14,7 @@ from notd.messages import UpdateCollectionTokensMessageContent
 @click.option('-a', '--collection-address', 'address', required=True, type=str)
 @click.option('-d', '--should-force', 'shouldForce', default=False, is_flag=True)
 async def run(address: str, shouldForce: Optional[bool]):
-    tokenQueue = SqsMessageQueue(region='eu-west-1', accessKeyId=os.environ['AWS_KEY'], accessKeySecret=os.environ['AWS_SECRET'], queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-work-queue')
+    tokenQueue = SqsMessageQueue(region='eu-west-1', accessKeyId=os.environ['AWS_KEY'], accessKeySecret=os.environ['AWS_SECRET'], queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-token-queue')
     await tokenQueue.connect()
     await tokenQueue.send_message(message=UpdateCollectionTokensMessageContent(address=address, shouldForce=shouldForce).to_message())
     await tokenQueue.disconnect()
