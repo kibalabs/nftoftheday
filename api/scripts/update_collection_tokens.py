843722df80c671e7434124ebb8b7496a2c30220f
Femi <ogunkolaobafemi@gmail.com>
2022-02-25 21:01:31 +0100

    .

--- api/scripts/update_collection_tokens.py
+++ api/scripts/update_collection_tokens.py
@@ -1,10 +1,6 @@
+import logging
 import os
 import sys
-
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-import logging
 from typing import Optional
 
 import asyncclick as click
@@ -14,17 +10,15 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from notd.messages import UpdateCollectionTokensMessageContent
 
 
-
 @click.command()
-@click.option('-a', '--address', 'address', required=True, type=str)
-@click.option('-f', '--should-force', 'shouldForce', required=False, type=bool )
+@click.option('-a', '--collection-address', 'address', required=True, type=str)
+@click.option('-d', '--should-force', 'shouldForce', default=False, is_flag=True)
 async def run(address: str, shouldForce: Optional[bool]):
-    tokenQueue = SqsMessageQueue(region='eu-west-1', accessKeyId=os.environ['AWS_KEY'], accessKeySecret=os.environ['AWS_SECRET'], queueUrl='https://sqs.eu-west-1.amazonaws.com/097520841056/notd-token-queue')
-
-    await tokenQueue.connect()
-    await tokenQueue.send_message(message=UpdateCollectionTokensMessageContent(address=address, shouldForce=shouldForce).to_message())
-    await tokenQueue.disconnect()
+    workQueue = SqsMessageQueue(region='us-east-1', accessKeyId=os.environ['AWS_KEY'], accessKeySecret=os.environ['AWS_SECRET'], queueUrl='https://sqs.us-east-1.amazonaws.com/113848722427/test1')
+    await workQueue.connect()
+    await workQueue.send_message(message=UpdateCollectionTokensMessageContent(address=address, shouldForce=shouldForce).to_message())
+    await workQueue.disconnect()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     run(_anyio_backend='asyncio')
