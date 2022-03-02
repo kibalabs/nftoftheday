fa5f3a4950c67b7ce20b08814a086e75f5806717
Femi <ogunkolaobafemi@gmail.com>
2022-03-02 23:14:11 +0100

    .

--- api/notd/store/schema_conversions.py
+++ api/notd/store/schema_conversions.py
@@ -4,7 +4,6 @@ from notd.model import TokenOwnership
 from notd.model import Block
 from notd.model import Collection
 from notd.model import TokenMetadata
-from notd.model import TokenOwner
 from notd.model import TokenTransfer
 from notd.store.schema import BlocksTable
 from notd.store.schema import TokenCollectionsTable
