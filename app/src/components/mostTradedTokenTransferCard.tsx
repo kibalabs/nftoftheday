82f0a7d50c40e39b3aaca71763f0d934132220df
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-03-08 13:48:57 +0100

    KIBA-1389: Update HomePage to link to CollectionPage and TokenPage (#275)
    Co-authored-by: Barakat Ajadi <barakat.a.ajadi@gmail.com>

--- app/src/components/mostTradedTokenTransferCard.tsx
+++ app/src/components/mostTradedTokenTransferCard.tsx
@@ -12,8 +12,7 @@ export const MostTradedTokenTransferCard = (props: MostTradedTokenTransferCardPr
     <NftCard
       label='Most Traded'
       subtitle={`Traded ${props.tradedToken.transferCount} times today`}
-      primaryButtonText='View on OpenSea'
-      primaryButtonTarget={`https://opensea.io/assets/${props.tradedToken.token.registryAddress}/${props.tradedToken.token.tokenId}?ref=0x18090cda49b21deaffc21b4f886aed3eb787d032`}
+      primaryButtonText='View Token'
       secondaryButtonText='View Tx'
       secondaryButtonTarget={`https://etherscan.io/tx/${props.tradedToken.latestTransfer.transactionHash}`}
       token={props.tradedToken.token}
