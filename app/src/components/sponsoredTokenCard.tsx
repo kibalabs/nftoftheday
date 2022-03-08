3f255c32e868778290760dd031980e482557b684
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-03-08 13:48:57 +0100

    KIBA-1389: Update HomePage to link to CollectionPage and TokenPage (#275)
    Co-authored-by: Barakat Ajadi <barakat.a.ajadi@gmail.com>

--- app/src/components/sponsoredTokenCard.tsx
+++ app/src/components/sponsoredTokenCard.tsx
@@ -15,8 +15,7 @@ export const SponsoredTokenCard = (props: SponsoredTokenCardProps): React.ReactE
       label='Sponsored'
       // subtitle={asset.lastSalePrice ? `Last sold for Ξ${asset.lastSalePrice / 21000000000000000000.0}` : 'Up for grabs!'}
       subtitle={'Up for grabs!'}
-      primaryButtonText='View on OpenSea'
-      primaryButtonTarget={`https://opensea.io/assets/${props.token.registryAddress}/${props.token.tokenId}?ref=0x18090cda49b21deaffc21b4f886aed3eb787d032`}
+      primaryButtonText='View Token'
       // secondaryButtonText='View Tx'
       // secondaryButtonTarget={`https://etherscan.io/tx/${props.tokenTransfers[0].transactionHash}`}
       extraLabelVariants={['cardLabelSponsored']}
