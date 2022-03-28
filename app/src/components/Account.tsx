72ce94ec48e268a45448ac04f335f0c7685f1ce3
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-03-28 10:08:27 +0100

    update

--- app/src/components/Account.tsx
+++ app/src/components/Account.tsx
@@ -9,7 +9,7 @@ export interface AccountViewProps {
 export const Account = (props: AccountViewProps): React.ReactElement => {
   return (
     <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true}>
-      <Box variant='rounded-borderColored' shouldClipContent={true} height='20px' width='20px'>
+      <Box variant='rounded' shouldClipContent={true} height='20px' width='20px'>
         <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${props.accountId}/image`} alternativeText='Avatar' />
       </Box>
       <Text>{truncateMiddle(props.accountId, 10)}</Text>
