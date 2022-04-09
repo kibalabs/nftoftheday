3b9c59452ffff6854d6769a2139a2f2267952cc4
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-04-09 09:31:13 +0100

    KIBA-1417: Add transaction button to token sales (#294)
    * KIBA-1417: Add transaction button to token sales
    
    * format Date
    
    * format date
    
    * update
    
    Co-authored-by: Barakat Ajadi <barakat.a.ajadi@gmail.com>

--- app/src/components/TokenSaleRow.tsx
+++ app/src/components/TokenSaleRow.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { dateToString } from '@kibalabs/core';
-import { Alignment, Box, Direction, KibaIcon, PaddingSize, ResponsiveHidingView, ScreenSize, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
+import { Alignment, Box, Direction, IconButton, KibaIcon, PaddingSize, ResponsiveHidingView, ScreenSize, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 import { TokenTransfer } from '../client/resources';
 import { Account } from './Account';
@@ -11,6 +10,44 @@ export interface ITokenSaleRowProps {
 }
 
 export const TokenSaleRow = (props: ITokenSaleRowProps): React.ReactElement => {
+  const formatDate = (date: Date): string => {
+    const seconds = Math.floor((new Date().valueOf() - date.valueOf()) / 1000);
+    let intervalType: string;
+
+    let interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) {
+      intervalType = 'year';
+    } else {
+      interval = Math.floor(seconds / 2592000);
+      if (interval >= 1) {
+        intervalType = 'month';
+      } else {
+        interval = Math.floor(seconds / 86400);
+        if (interval >= 1) {
+          intervalType = 'day';
+        } else {
+          interval = Math.floor(seconds / 3600);
+          if (interval >= 1) {
+            intervalType = 'hour';
+          } else {
+            interval = Math.floor(seconds / 60);
+            if (interval >= 1) {
+              intervalType = 'minute';
+            } else {
+              interval = seconds;
+              intervalType = 'second';
+            }
+          }
+        }
+      }
+    }
+
+    if (interval > 1 || interval === 0) {
+      intervalType += 's';
+    }
+    return `${interval} ${intervalType}`;
+  };
+
   return (
     <Stack direction={Direction.Vertical} shouldWrapItems={true} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
       <ResponsiveHidingView hiddenBelow={ScreenSize.Medium}>
@@ -36,8 +73,11 @@ export const TokenSaleRow = (props: ITokenSaleRowProps): React.ReactElement => {
               </Stack.Item>
               <Stack.Item baseSize='12rem' alignment={Alignment.Center}>
                 <Text alignment={TextAlignment.Center}>
-                  {dateToString(props.tokenTransfer.blockDate, 'HH:mm')}
+                  {`${formatDate(props.tokenTransfer.blockDate)} ago`}
                 </Text>
+              </Stack.Item>
+              <Stack.Item alignment={Alignment.Center}>
+                <IconButton icon={<KibaIcon iconId='ion-open-outline' />} target={`https://etherscan.io/tx/${props.tokenTransfer.transactionHash}`} />
               </Stack.Item>
             </Stack>
           </Box>
@@ -55,13 +95,16 @@ export const TokenSaleRow = (props: ITokenSaleRowProps): React.ReactElement => {
               <Spacing variant={PaddingSize.Wide2} />
               <Stack.Item alignment={Alignment.Center}>
                 <Text alignment={TextAlignment.Left}>
-                  {dateToString(props.tokenTransfer.blockDate, 'HH:mm')}
+                  {`${formatDate(props.tokenTransfer.blockDate)} ago`}
                 </Text>
+              </Stack.Item>
+              <Stack.Item alignment={Alignment.Center}>
+                <IconButton icon={<KibaIcon iconId='ion-open-outline' />} target={`https://etherscan.io/tx/${props.tokenTransfer.transactionHash}`} />
               </Stack.Item>
             </Stack>
           </Box>
           <Box variant='tokenSaleRow'>
-            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Start} contentAlignment={Alignment.Start}>
+            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
               <Stack.Item baseSize='5rem' alignment={Alignment.Center}>
                 <Account accountId={props.tokenTransfer?.fromAddress} />
               </Stack.Item>
