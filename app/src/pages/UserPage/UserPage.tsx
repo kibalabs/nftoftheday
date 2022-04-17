37b71e9dbc0a78b62cf0b9fd4a6c65a27608e101
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2022-04-17 01:09:11 +0100

    user address

--- app/src/pages/UserPage/UserPage.tsx
+++ app/src/pages/UserPage/UserPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Alignment, Direction, PaddingSize, Stack, Text } from '@kibalabs/ui-react';
+import { truncateMiddle } from '@kibalabs/core';
+import { Alignment, Box, Direction, Image, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
 
 import { useAccount } from '../../AccountContext';
 import { CollectionToken } from '../../client/resources';
@@ -32,6 +33,15 @@ export const UserPage = (): React.ReactElement => {
 
   return (
     <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} shouldAddGutters={true} paddingVertical={PaddingSize.Wide2} isScrollableHorizontally={true}>
+      { account?.address && (
+        <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+          <Box variant='rounded' shouldClipContent={true} height='40px' width='40px'>
+            <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${account?.address}/image`} alternativeText='Avatar' />
+          </Box>
+          <Text variant='header2'>{truncateMiddle(account?.address, 15)}</Text>
+        </Stack>
+      )}
+      <Spacing variant={PaddingSize.Wide} />
       <Text variant='header3'>Your Tokens</Text>
       <Stack direction={Direction.Horizontal}contentAlignment={Alignment.Center} childAlignment={Alignment.Center} shouldAddGutters={true}>
         {ownerTokens && ownerTokens.map((ownerToken: CollectionToken, index: number) : React.ReactElement => (
