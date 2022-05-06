560bdc9e4f677cf0db4f0685a0d1dc73e4413bf6
Femi <ogunkolaobafemi@gmail.com>
2022-05-06 21:13:29 +0100

    reviewed by femi

--- api/notd/date_util.py
+++ api/notd/date_util.py
@@ -1,9 +1,6 @@
 import datetime
 from typing import Optional
 
-from core.util import date_util
-
-
 def date_hour_from_datetime(dt: Optional[datetime.datetime] = None) -> datetime.datetime:
     dt = dt if dt is not None else datetime.datetime.utcnow()
     return dt.replace(minute=0, second=0, microsecond=0)
