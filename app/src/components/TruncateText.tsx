3e13eec2c572fed3178266eab8d82ad8630350db
Barakat Ajadi <briannaajadi03@gmail.com>
2022-06-08 13:21:13 +0100

    update

--- app/src/components/TruncateText.tsx
+++ app/src/components/TruncateText.tsx
@@ -14,7 +14,7 @@ export const TruncateText = (props: TruncateTextProps): React.ReactElement => {
     setShouldTruncateText(!shouldTruncateText);
   };
   return (
-    <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
+    <Stack direction={Direction.Vertical} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
       <Box maxHeight={shouldTruncateText ? `calc(${theme.texts.default['line-height']} * 3)` : undefined} shouldClipContent={true}>
         <MarkdownText textAlignment={TextAlignment.Center} source={props.markdownText} />
       </Box>
