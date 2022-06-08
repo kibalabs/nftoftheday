50b79bdeeebfcd4fd5c044c96bb517fc048915c2
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-06-08 17:55:06 +0100

    KIBA-1495: only show read more button on CollectionPage when relevant + center align description text (#350)
    * Display read more button if text is more than max
    
    * update
    
    * update

--- app/src/components/TruncateText.tsx
+++ app/src/components/TruncateText.tsx
@@ -14,14 +14,12 @@ export const TruncateText = (props: TruncateTextProps): React.ReactElement => {
     setShouldTruncateText(!shouldTruncateText);
   };
   return (
-    <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
+    <Stack direction={Direction.Vertical} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
       <Box maxHeight={shouldTruncateText ? `calc(${theme.texts.default['line-height']} * 3)` : undefined} shouldClipContent={true}>
         <MarkdownText textAlignment={TextAlignment.Center} source={props.markdownText} />
       </Box>
-      {shouldTruncateText ? (
-        <Button variant='small' text={'read more'} onClicked={onToggleClicked} />
-      ) : (
-        <Button variant='small' text={'read less'} onClicked={onToggleClicked} />
+      {props.markdownText.length >= props.maximumCharacters && (
+        <Button variant='small' text={shouldTruncateText ? 'read more' : 'read less'} onClicked={onToggleClicked} />
       )}
     </Stack>
   );
