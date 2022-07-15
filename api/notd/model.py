f86e6e6fa3921f70c6f7cd4df8044f61bf844151
Krishan Patel <krishan711@gmail.com>
2022-07-15 16:46:56 +0100

    Fix: make value nullable in tokenAttribute model (#385)
    * .
    
    * .
    
    * .

--- api/notd/model.py
+++ api/notd/model.py
@@ -256,18 +256,21 @@ class Airdrop:
     claimTokenKey: Token
     claimUrl: str
 
+
 @dataclasses.dataclass
 class RetrievedTokenAttribute:
     registryAddress: str
     tokenId: str
     name: str
-    value: str
+    value: Optional[str]
+
 
 @dataclasses.dataclass
 class TokenAttribute(RetrievedTokenAttribute):
     tokenAttributeId: int
     createdDate: datetime.datetime
     updatedDate: datetime.datetime
+
 
 @dataclasses.dataclass
 class RetrievedTokenListing:
