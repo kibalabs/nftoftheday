04620a668c0d8ba81b986ce0ec8a6e19bc6481fb
Femi <ogunkolaobafemi@gmail.com>
2022-07-20 11:27:03 +0100

    .

--- api/notd/looksrare.py
+++ api/notd/looksrare.py
@@ -16,34 +16,38 @@ async def test():
         'isOrderAsk':'true',
         'collection':'0xbce3781ae7ca1a5e050bd9c4c77369867ebc307e',
         'status[]':'VALID',
-        'pagination[first]': 150,
+        'pagination[first]': 100,
         'sort':'PRICE_ASC',
     }
-    response = await requester.get(url='https://api.looksrare.org/api/v1/orders', dataDict=queryData, timeout=30)
-    responseJson = response.json()
-    assetListings = []
-    for order in responseJson['data']:
-        startDate = datetime.datetime.utcfromtimestamp(order["startTime"])
-        endDate = datetime.datetime.utcfromtimestamp(order["endTime"])
-        currentPrice = int(order["price"])
-        #Note(Femi-Ogunkola): Confirm what the signer address is
-        offererAddress = order['signer']
-        sourceId = order["hash"]
-        isValueNative = order["currencyAddress"] == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-        listing = RetrievedTokenListing(
-            registryAddress=order['collectionAddress'],
-            tokenId=order['tokenId'],
-            startDate=startDate,
-            endDate=endDate,
-            isValueNative=isValueNative,
-            value=currentPrice,
-            offererAddress=offererAddress,
-            source='LooksRare',
-            sourceId=sourceId,
-        )
-        print(offererAddress)
-        assetListings.append(listing)
-    #print(assetListings)
+    flag = True
+    while flag:
+        response = await requester.get(url='https://api.looksrare.org/api/v1/orders', dataDict=queryData, timeout=30)
+        responseJson = response.json()
+        assetListings = []
+        if len(responseJson['data']) == 0:
+            flag = False
+            break
+        for order in responseJson['data']:
+            startDate = datetime.datetime.utcfromtimestamp(order["startTime"])
+            endDate = datetime.datetime.utcfromtimestamp(order["endTime"])
+            currentPrice = int(order["price"])
+            #Note(Femi-Ogunkola): Confirm what the signer address is
+            offererAddress = order['signer']
+            sourceId = order["hash"]
+            isValueNative = order["currencyAddress"] == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+            listing = RetrievedTokenListing(
+                registryAddress=order['collectionAddress'],
+                tokenId=order['tokenId'],
+                startDate=startDate,
+                endDate=endDate,
+                isValueNative=isValueNative,
+                value=currentPrice,
+                offererAddress=offererAddress,
+                source='LooksRare',
+                sourceId=sourceId,
+            )
+            assetListings.append(listing)
+        queryData['pagination[cursor]'] = order['hash']
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
