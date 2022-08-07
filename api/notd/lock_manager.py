7454c79060ef6a4408c820f32c546fc7c26eebc6
Femi <ogunkolaobafemi@gmail.com>
2022-08-07 13:17:09 +0100

    .

--- api/notd/lock_manager.py
+++ api/notd/lock_manager.py
@@ -1,3 +1,12 @@
+from core.util import date_util
+from notd.store.retriever import Retriever
 class LockManager:
-    def __init__(self) -> None:
+    def __init__(self, retriever: Retriever) -> None:
+        self.retriever = retriever
+        pass
+    
+
+    async def acquire_lock(self, name: str, timeoutSeconds: str, expirySeconds: str):
+        startDate = date_util.datetime_from_now()
+
         pass
