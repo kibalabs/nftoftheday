55f30deb505ec4791dcddc520e9224cb811dfb19
Krishan Patel <krishan711@gmail.com>
2022-08-26 16:28:03 +0100

    .

--- api/notd/token_listing_processor.py
+++ api/notd/token_listing_processor.py
@@ -23,87 +23,87 @@ class TokenListingProcessor:
         self.lockManager = lockManager
 
     async def get_opensea_listings_for_tokens(self, registryAddress: str, tokenIds: List[str]) -> List[RetrievedTokenListing]:
-        # async with self.lockManager.with_lock(name='opensea-requester', timeoutSeconds=(400 * 10), expirySeconds=(400 * 2)):
         listings = []
-        for index, chunkedTokenIds in enumerate(list_util.generate_chunks(lst=tokenIds, chunkSize=30)):
-            logging.stat('RETRIEVE_LISTINGS_OPENSEA', registryAddress, index)
-            queryData = {
-                'token_ids': chunkedTokenIds,
-                'asset_contract_address': registryAddress,
-                'include_orders': True,
-                'limit': len(chunkedTokenIds),
-            }
-            response = await self.openseaRequester.get(url='https://api.opensea.io/api/v1/assets', dataDict=queryData, timeout=30)
-            responseJson = response.json()
-            for asset in responseJson['assets']:
-                assetListings = []
-                for sellOrder in (asset.get('sell_orders') or []):
-                    side = sellOrder["side"]
-                    if side != 1:
-                        continue
-                    saleKind = sellOrder["sale_kind"]
-                    if saleKind != 0:
-                        # NOTE(krishan711): what should happen here?
-                        continue
-                    cancelled = sellOrder["cancelled"]
-                    if cancelled:
-                        # NOTE(krishan711): what should happen here?
-                        continue
-                    startDate = datetime.datetime.utcfromtimestamp(sellOrder["listing_time"])
-                    endDate = datetime.datetime.utcfromtimestamp(sellOrder["expiration_time"])
-                    currentPrice = int(sellOrder["current_price"].split('.')[0])
-                    offererAddress = chain_util.normalize_address(sellOrder["maker"]["address"])
-                    sourceId = sellOrder["order_hash"]
-                    isValueNative = sellOrder["payment_token_contract"]["symbol"] == "ETH"
-                    listing = RetrievedTokenListing(
-                        registryAddress=registryAddress,
-                        tokenId=asset['token_id'],
-                        startDate=startDate,
-                        endDate=endDate,
-                        isValueNative=isValueNative,
-                        value=currentPrice,
-                        offererAddress=offererAddress,
-                        source='opensea-wyvern',
-                        sourceId=sourceId,
-                    )
-                    assetListings.append(listing)
-                for seaportSellOrder in (asset.get('seaport_sell_orders') or []):
-                    side = seaportSellOrder["side"]
-                    if side != 'ask':
-                        continue
-                    orderType = seaportSellOrder["order_type"]
-                    if orderType != 'basic':
-                        # NOTE(krishan711): what should happen here?
-                        continue
-                    cancelled = seaportSellOrder["cancelled"]
-                    if cancelled:
-                        # NOTE(krishan711): what should happen here?
-                        continue
-                    startDate = datetime.datetime.utcfromtimestamp(seaportSellOrder["listing_time"])
-                    endDate = datetime.datetime.utcfromtimestamp(seaportSellOrder["expiration_time"])
-                    currentPrice = int(seaportSellOrder["current_price"].split('.')[0])
-                    offererAddress = seaportSellOrder["maker"]["address"]
-                    sourceId = seaportSellOrder["order_hash"]
-                    isValueNative = True
-                    # NOTE(krishan711): should isValueNative and value be calculated using considerations?
-                    listing = RetrievedTokenListing(
-                        registryAddress=registryAddress,
-                        tokenId=asset['token_id'],
-                        startDate=startDate,
-                        endDate=endDate,
-                        isValueNative=isValueNative,
-                        value=currentPrice,
-                        offererAddress=chain_util.normalize_address(offererAddress),
-                        source='opensea-seaport',
-                        sourceId=sourceId,
-                    )
-                    assetListings.append(listing)
-                if len(assetListings) > 0:
-                    # NOTE(krishan711): take the lowest one
-                    sortedAssetListings = sorted(assetListings, key=lambda listing: listing.value, reverse=False)
-                    listings.append(sortedAssetListings[0])
-            # NOTE(krishan711): sleep to avoid opensea limits
-            await asyncio.sleep(0.25)
+        async with self.lockManager.with_lock(name='opensea-requester', timeoutSeconds=(400 * 10), expirySeconds=(400 * 2)):
+            for index, chunkedTokenIds in enumerate(list_util.generate_chunks(lst=tokenIds, chunkSize=30)):
+                logging.stat('RETRIEVE_LISTINGS_OPENSEA', registryAddress, index)
+                queryData = {
+                    'token_ids': chunkedTokenIds,
+                    'asset_contract_address': registryAddress,
+                    'include_orders': True,
+                    'limit': len(chunkedTokenIds),
+                }
+                response = await self.openseaRequester.get(url='https://api.opensea.io/api/v1/assets', dataDict=queryData, timeout=30)
+                responseJson = response.json()
+                for asset in responseJson['assets']:
+                    assetListings = []
+                    for sellOrder in (asset.get('sell_orders') or []):
+                        side = sellOrder["side"]
+                        if side != 1:
+                            continue
+                        saleKind = sellOrder["sale_kind"]
+                        if saleKind != 0:
+                            # NOTE(krishan711): what should happen here?
+                            continue
+                        cancelled = sellOrder["cancelled"]
+                        if cancelled:
+                            # NOTE(krishan711): what should happen here?
+                            continue
+                        startDate = datetime.datetime.utcfromtimestamp(sellOrder["listing_time"])
+                        endDate = datetime.datetime.utcfromtimestamp(sellOrder["expiration_time"])
+                        currentPrice = int(sellOrder["current_price"].split('.')[0])
+                        offererAddress = chain_util.normalize_address(sellOrder["maker"]["address"])
+                        sourceId = sellOrder["order_hash"]
+                        isValueNative = sellOrder["payment_token_contract"]["symbol"] == "ETH"
+                        listing = RetrievedTokenListing(
+                            registryAddress=registryAddress,
+                            tokenId=asset['token_id'],
+                            startDate=startDate,
+                            endDate=endDate,
+                            isValueNative=isValueNative,
+                            value=currentPrice,
+                            offererAddress=offererAddress,
+                            source='opensea-wyvern',
+                            sourceId=sourceId,
+                        )
+                        assetListings.append(listing)
+                    for seaportSellOrder in (asset.get('seaport_sell_orders') or []):
+                        side = seaportSellOrder["side"]
+                        if side != 'ask':
+                            continue
+                        orderType = seaportSellOrder["order_type"]
+                        if orderType != 'basic':
+                            # NOTE(krishan711): what should happen here?
+                            continue
+                        cancelled = seaportSellOrder["cancelled"]
+                        if cancelled:
+                            # NOTE(krishan711): what should happen here?
+                            continue
+                        startDate = datetime.datetime.utcfromtimestamp(seaportSellOrder["listing_time"])
+                        endDate = datetime.datetime.utcfromtimestamp(seaportSellOrder["expiration_time"])
+                        currentPrice = int(seaportSellOrder["current_price"].split('.')[0])
+                        offererAddress = seaportSellOrder["maker"]["address"]
+                        sourceId = seaportSellOrder["order_hash"]
+                        isValueNative = True
+                        # NOTE(krishan711): should isValueNative and value be calculated using considerations?
+                        listing = RetrievedTokenListing(
+                            registryAddress=registryAddress,
+                            tokenId=asset['token_id'],
+                            startDate=startDate,
+                            endDate=endDate,
+                            isValueNative=isValueNative,
+                            value=currentPrice,
+                            offererAddress=chain_util.normalize_address(offererAddress),
+                            source='opensea-seaport',
+                            sourceId=sourceId,
+                        )
+                        assetListings.append(listing)
+                    if len(assetListings) > 0:
+                        # NOTE(krishan711): take the lowest one
+                        sortedAssetListings = sorted(assetListings, key=lambda listing: listing.value, reverse=False)
+                        listings.append(sortedAssetListings[0])
+                # NOTE(krishan711): sleep to avoid opensea limits
+                await asyncio.sleep(0.25)
         return listings
 
     async def get_changed_opensea_token_listings_for_collection(self, address: str, startDate: datetime.datetime) -> List[str]:
