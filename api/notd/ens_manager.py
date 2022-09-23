b5e7bdf456e4a21fa9bab30924e0d834263a1372
Femi <ogunkolaobafemi@gmail.com>
2022-09-23 11:29:19 +0100

    .

--- api/notd/ens_manager.py
+++ api/notd/ens_manager.py
@@ -1,12 +1,22 @@
 
 from typing import Optional
+from notd.store.retriever import Retriever
 from notd.model import AccountEnsName
 
 
 class EnsManager:
 
-    def __init__(self) -> None:
+    def __init__(self, retriever: Retriever) -> None:
+        self.retriever = retriever
         pass
 
-    async def get_account_ens_name() -> Optional[AccountEnsName]:
+    async def get_account_ens_name(self, accountAddress: str) -> Optional[AccountEnsName]:
+        accountEnsName = await self.retriever.get_account_ens_name_by_account_address(accountAddress=accountAddress)
+        if accountEnsName.updatedDate < 3:
+            return accountEnsName
+        else:
+            await self.update_account_ens_name(accountAddress=accountAddress)
+        
+    
+    async def update_account_ens_name(self, accountAddress: str) -> None:
         pass
