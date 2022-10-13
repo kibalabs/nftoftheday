ee568506364d9276fca84ded9728e79d92dd71db
Femi <ogunkolaobafemi@gmail.com>
2022-10-13 15:14:46 +0100

    .

--- dbschema/1.tables.sql
+++ dbschema/1.tables.sql
@@ -441,3 +441,21 @@ CREATE INDEX tbl_collection_overlaps_registry_address ON tbl_collection_overlaps
 CREATE INDEX tbl_collection_overlaps_other_registry_address ON tbl_collection_overlaps (other_registry_address);
 CREATE INDEX tbl_collection_overlaps_owner_address ON tbl_collection_overlaps (owner_address);
 CREATE INDEX tbl_collection_overlaps_registry_token_count ON tbl_collection_overlaps (registry_token_count);
+
+
+CREATE TABLE tbl_gallery_badges (
+    id BIGSERIAL PRIMARY KEY,
+    created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    registry_address TEXT NOT NULL,
+    owner_address TEXT NOT NULL,
+    badge_key TEXT NOT NULL,
+    achieved_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+);
+CREATE UNIQUE INDEX tbl_gallery_badges_owner_address_registry_address_badge_key on tbl_gallery_badges (owner_address, registry_address, badge_key);
+CREATE INDEX tbl_gallery_badges_created_date ON tbl_gallery_badges (created_date);
+CREATE INDEX tbl_gallery_badges_updated_date ON tbl_gallery_badges (updated_date);
+CREATE INDEX tbl_gallery_badges_registry_address ON tbl_gallery_badges (registry_address);
+CREATE INDEX tbl_gallery_badges_badge_key ON tbl_gallery_badges (badge_key);
+CREATE INDEX tbl_gallery_badges_owner_address ON tbl_gallery_badges (owner_address);
+CREATE INDEX tbl_gallery_badges_achieved_date ON tbl_gallery_badges (achieved_date);
