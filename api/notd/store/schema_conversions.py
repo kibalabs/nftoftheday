39e440ee04f4325f1b05b088b687195a8036f2e4
Femi <ogunkolaobafemi@gmail.com>
2022-11-03 11:38:11 +0100

    .

--- api/notd/store/schema_conversions.py
+++ api/notd/store/schema_conversions.py
@@ -360,7 +360,6 @@ def gallery_badge_holder_from_row(rowMapping: RowMapping) -> GalleryBadgeHolder:
         achievedDate=rowMapping[GalleryBadgeHoldersTable.c.achievedDate],
     )
 
-
 def gallery_assigned_badge_holder_from_row(rowMapping: RowMapping) -> GalleryAssignedBadgeHolder:
     return GalleryAssignedBadgeHolder(
         galleryAssignedBadgeHolderId=rowMapping[GalleryAssignedBadgeHoldersTable.c.galleryAssignedBadgeHolderId],
