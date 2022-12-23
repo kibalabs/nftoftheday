412d6a183a147de20a0296c86fa42bde8ffcc643
Femi <ogunkolaobafemi@gmail.com>
2022-12-23 21:55:21 +0100

    .

--- api/notd/token_staking_processor.py
+++ api/notd/token_staking_processor.py
@@ -18,7 +18,7 @@ class TokenStakingProcessor:
 
     async def calculate_staked_creepz_tokens(self, registryAddress: str) -> List[RetrievedTokenStaking]:
         stakedQuery = (
-        sqlalchemy.select(TokenTransfersTable.c.tokenId, TokenTransfersTable.c.toAddress, BlocksTable.c.blockDate)
+        sqlalchemy.select(TokenTransfersTable.c.tokenId, TokenTransfersTable.c.fromAddress, BlocksTable.c.blockDate)
         .join(BlocksTable, BlocksTable.c.blockNumber == TokenTransfersTable.c.blockNumber)
         .where(TokenTransfersTable.c.registryAddress == registryAddress)
         .where(TokenTransfersTable.c.toAddress == CREEPZ_STAKING_CONTRACT)
@@ -38,15 +38,13 @@ class TokenStakingProcessor:
         # currentlyStakedTokens: Dict[str, Tuple[str, datetime.datetime]] = {}
         currentlyStakedTokens = defaultdict()
         for tokenId, ownerAddress, blockDate in stakedTokens:
-            if tokenId == '9135':
-                print('present')
             currentlyStakedTokens[tokenId] = (ownerAddress, blockDate)
 
         for tokenId, ownerAddress, blockDate in unStakedTokens:
-            if tokenId == '9135':
-                print('absent')
+            if tokenId not in currentlyStakedTokens.keys():
+                continue
             if currentlyStakedTokens[tokenId][1] < blockDate:
                 del currentlyStakedTokens[tokenId]
-
         retrievedStakedTokens = [RetrievedTokenStaking(registryAddress=registryAddress, tokenId=tokenId, stakingAddress=CREEPZ_STAKING_CONTRACT, ownerAddress=ownerAddress, stakingDate=stakingDate) for tokenId, (ownerAddress, stakingDate) in currentlyStakedTokens.items()]
+        print(len(retrievedStakedTokens))
         return retrievedStakedTokens
