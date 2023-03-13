30b8325014e88d124604de1ad7eccf8514e2ecc8
Femi <ogunkolaobafemi@gmail.com>
2023-03-13 13:43:30 +0100

    .

--- dbschema/2.views.sql
+++ dbschema/2.views.sql
@@ -11,6 +11,15 @@ CREATE INDEX mvw_user_registry_first_ownerships_registry_address ON mvw_user_reg
 
 CREATE MATERIALIZED VIEW mvw_user_registry_ordered_ownerships AS (
     (
+        SELECT tbl_token_metadatas.registry_address, tbl_token_metadatas.token_id, tbl_token_stakings.owner_address, 1 AS quantity, row_number() OVER (
+            PARTITION BY tbl_token_stakings.registry_address, tbl_token_stakings.owner_address
+            ORDER BY tbl_token_stakings.staked_date, tbl_token_stakings.token_id ASC
+        ) AS owner_token_index
+        FROM tbl_token_metadatas
+        JOIN tbl_token_stakings ON tbl_token_stakings.token_id = tbl_token_metadatas.token_id
+            AND tbl_token_stakings.registry_address = tbl_token_metadatas.registry_address
+        WHERE tbl_token_stakings.registry_address = ANY(array(select registry_address from tbl_gallery_customers))
+    ) UNION (
         SELECT tbl_token_metadatas.registry_address, tbl_token_metadatas.token_id, tbl_token_ownerships.owner_address, 1 AS quantity, row_number() OVER (
             PARTITION BY tbl_token_ownerships.registry_address, tbl_token_ownerships.owner_address
             ORDER BY tbl_token_ownerships.transfer_date, tbl_token_ownerships.token_id ASC
@@ -18,8 +27,8 @@ CREATE MATERIALIZED VIEW mvw_user_registry_ordered_ownerships AS (
         FROM tbl_token_metadatas
         JOIN tbl_token_ownerships ON tbl_token_ownerships.token_id = tbl_token_metadatas.token_id
             AND tbl_token_ownerships.registry_address = tbl_token_metadatas.registry_address
-            AND tbl_token_ownerships.owner_address != '0xC3503192343EAE4B435E4A1211C5d28BF6f6a696'
         WHERE tbl_token_ownerships.registry_address = ANY(array(select registry_address from tbl_gallery_customers))
+        AND ((tbl_token_ownerships.registry_address, tbl_token_ownerships.token_id) not in (select registry_address, token_id from tbl_token_stakings))
     ) UNION (
         SELECT tbl_token_metadatas.registry_address, tbl_token_metadatas.token_id, tbl_token_multi_ownerships.owner_address, tbl_token_multi_ownerships.quantity, row_number() OVER (
             PARTITION BY tbl_token_multi_ownerships.registry_address, tbl_token_multi_ownerships.owner_address
@@ -29,17 +38,8 @@ CREATE MATERIALIZED VIEW mvw_user_registry_ordered_ownerships AS (
         JOIN tbl_token_multi_ownerships ON tbl_token_multi_ownerships.token_id = tbl_token_metadatas.token_id
             AND tbl_token_multi_ownerships.registry_address = tbl_token_metadatas.registry_address
             AND tbl_token_multi_ownerships.quantity > 0
-            AND tbl_token_multi_ownerships.owner_address != '0xC3503192343EAE4B435E4A1211C5d28BF6f6a696'
         WHERE tbl_token_multi_ownerships.registry_address = ANY(array(select registry_address from tbl_gallery_customers))
-    ) UNION (
-        SELECT tbl_token_metadatas.registry_address, tbl_token_metadatas.token_id, tbl_token_stakings.owner_address, 1 AS quantity, row_number() OVER (
-            PARTITION BY tbl_token_stakings.registry_address, tbl_token_stakings.owner_address
-            ORDER BY tbl_token_stakings.staked_date, tbl_token_stakings.token_id ASC
-        ) AS owner_token_index
-        FROM tbl_token_metadatas
-        JOIN tbl_token_stakings ON tbl_token_stakings.token_id = tbl_token_metadatas.token_id
-            AND tbl_token_stakings.registry_address = tbl_token_metadatas.registry_address
-        WHERE tbl_token_stakings.registry_address = ANY(array(select registry_address from tbl_gallery_customers))
+        AND ((tbl_token_multi_ownerships.registry_address, tbl_token_multi_ownerships.token_id) not in (select registry_address, token_id from tbl_token_stakings))
     )
 );
 CREATE UNIQUE INDEX mvw_user_registry_ordered_ownerships_registry_address_token_id_owner_address ON mvw_user_registry_ordered_ownerships (registry_address, token_id, owner_address);
